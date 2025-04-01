b23ca95d2b2e9c33b303f1d3e7eef542c926c946
Andrew Heberle <andrew.heberle@gmail.com>
2025-04-01 08:21:11 +0800

    Update example

--- example_test.go
+++ example_test.go
@@ -11,7 +11,13 @@ func ExampleNewFlag() {
 	size := iecbyte.NewFlag(1024 * 1024)
 
 	flag.Var(&size, "size", "Size in IEC bytes")
-	flag.Parse()
+	//
+	// In this example flag.Parse() is commented out as this forms part of the tests
+	// of this module, so parsing the command line flags is disabled.
+	//
+	// In a real program you would need to call flag.Parse()
+	//
+	// flag.Parse()
 
 	fmt.Printf("Size is %s\n", size)
 	// Output: Size is 1Mi
